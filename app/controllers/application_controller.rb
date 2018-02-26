634b2ac201591bbc6758c3f42ac8b19e67e1c67a
Igisha <cubro994@hotmail.com>
2018-02-26 22:52:02 +0000

    Added profiles create action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery with: :exception
   
   # Whitelist the following form fields so that we can process them, if coming from a Devise sign up form
@@ -8,4 +9,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
   end
+end
 end
